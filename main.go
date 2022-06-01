cdb73ea691520cf82d5ea33056954e4d646b7ce9
Jason-Leiby <jason.leiby@ibm.com>
2022-06-01 07:50:09 -0600

    cleanup, add docs

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"terraform-provider-logship/provider"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider.New})
